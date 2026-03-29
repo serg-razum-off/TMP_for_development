780a82e02c62e2419d507a9ef99554e5da73442a
SR <sergiy.razumov@gmail.com>
2026-03-29 15:16:46 +0300

    SR[2026-03-29T15:16:46]: Umbrella support gesign started

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("orders/", include("orders.urls")),
+    path("", RedirectView.as_view(url="/orders/", permanent=False)),
 ]
+
